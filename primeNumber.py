e263d5e4fe30b688d0be82e244c9891448f9026c
justin-kim3000 <suc40@naver.com>
2022-08-25 22:24:53 +0900

    ing

---
+++ primeNumber.py
@@ -0,0 +1,9 @@
+inputNumber = int(input())
+inputNumbers = map(int, input().split())
+count = 0
+print(inputNumbers)
+# for i in inputNumbers:
+#     if i>1 and i%2!=0 and i%3!=0 and i%5!=0:
+#         count += 1
+#     elif i==2 or i ==3 or i==5 or i==7 :
+#         count += 1
