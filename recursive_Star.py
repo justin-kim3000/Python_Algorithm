07da03e0eadc3a766f522eebc2603fd4b28ba1d5
justin-kim3000 <suc40@naver.com>
2022-08-28 21:35:25 +0900

    recursive-ing

---
+++ recursive_Star.py
@@ -0,0 +1,23 @@
+from math import sqrt
+import math
+
+"""
+******
+* ** *
+******
+******
+* ** *
+******
+"""
+
+# inputNumber = int(input())
+# sqrtInputNumber = math.sqrt(inputNumber)
+
+# for i in range(1, 10):
+#     print('***'*i)
+#     for j in range(1, 10):
+#         print('* *'*j)
+
+print('***')
+print('* *')
+print('***')
