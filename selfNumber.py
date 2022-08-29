f0f5dd39ba3361314edaec155b0c97e3b412f681
justin-kim3000 <suc40@naver.com>
2022-08-29 23:28:50 +0900

    loop-ing

---
+++ selfNumber.py
@@ -0,0 +1,8 @@
+inputNumber = int(input())
+maxNumber = 100
+while inputNumber < maxNumber:
+    if inputNumber < maxNumber:
+        print(inputNumber + inputNumber//maxNumber +
+              inputNumber//100 + inputNumber//10)
+
+    inputNumber += 1
