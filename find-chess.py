771757851ea93ab28e5ba620b8eef6ce6167bac5
justin-kim3000 <suc40@naver.com>
2022-09-05 00:23:12 +0900

    think easily

--- find-chess.py
+++ find-chess.py
@@ -1,1 +1,20 @@
 # 킹 1 / 퀸 1 / 룩 2 / 비숍 2 / 나이트 2 / 폰 8
+
+def find_chess(king=1, queen=1, ruck=2, bishop=2, knight=2, pawn=8):
+    if (king and queen == 1):
+        return king, queen
+    elif (king and queen != 1):
+        return -(king - 1), -(queen - 1)
+    elif (ruck and bishop and knight == 2):
+        return ruck, bishop, knight
+    elif (ruck and bishop and knight != 2):
+        return -(ruck - 2), -(bishop - 2), -(knight - 2)
+    elif (pawn == 8):
+        return pawn
+    elif (pawn != 8):
+        return -(pawn - 8)
+    else:
+        print("error")
+
+
+print(find_chess(5, 2, 2, 2, 7))
