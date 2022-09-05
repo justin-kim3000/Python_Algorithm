564b6f3295b196d406d37a0428e6c864655d1c4e
justin-kim3000 <suc40@naver.com>
2022-09-05 23:40:44 +0900

    think easily

--- findShortestWay.py
+++ findShortestWay.py
@@ -6,3 +6,12 @@ x, y, w, h = map(int, input().split())
 
 def findAbsoluteValue(x, y, w, h):
     result = []
+    result.append(abs(h-y))
+    result.append(abs(w-x))
+    result.append(abs(x))
+    result.append(abs(y))
+
+    return min(result)
+
+
+print(findAbsoluteValue(x, y, w, h))
