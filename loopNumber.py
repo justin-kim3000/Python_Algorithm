60ef4d8efb2e1c347edc0c6f51ab2e50cb1db184
Justin_Kim <65004984+justin-kim3000@users.noreply.github.com>
2022-09-07 14:41:34 +0900

    use_loop

---
+++ loopNumber.py
@@ -0,0 +1,14 @@
+# N = int(input())
+
+# for i in range(N):
+#     A, B = map(int, input().split())
+#     print(A+B)
+
+
+while(True):
+    A, B = map(int, input().split())
+
+    if (A and B) == 0:
+        break
+    else:
+        print(A+B)
