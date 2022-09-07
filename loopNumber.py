7b9cdb8627fbcb9e700a4fefcc5ba62b125b29ed
justin-kim3000 <suc40@naver.com>
2022-09-07 22:23:02 +0900

    sys-stdin-readline

--- loopNumber.py
+++ loopNumber.py
@@ -1,11 +1,11 @@
-# N = int(input())
+N = int(input())
 
-# for i in range(N):
-#     A, B = map(int, input().split())
-#     print(A+B)
+for i in range(N):
+    A, B = map(int, sys.stdin.readline().split())
+    print(A+B)
 
 
-while(True):
+while (True):
     A, B = map(int, input().split())
 
     if (A and B) == 0:
