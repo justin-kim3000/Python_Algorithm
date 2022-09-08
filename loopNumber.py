0459e694c366459ff780f1f077bb3cf4a70d03d4
Justin_Kim <65004984+justin-kim3000@users.noreply.github.com>
2022-09-08 09:22:30 +0900

    for-while

--- loopNumber.py
+++ loopNumber.py
@@ -1,14 +1,24 @@
 N = int(input())
 
 for i in range(N):
-    A, B = map(int, sys.stdin.readline().split())
+    A, B = map(int, input().split())
     print(A+B)
 
+# while(True):
+#     A, B = map(int, input().split())
 
-while (True):
-    A, B = map(int, input().split())
+#     if (A and B) == 0:
+#         break
+#     else:
+#         print(A+B)
 
-    if (A and B) == 0:
-        break
-    else:
-        print(A+B)
+# for i in range(1, N+1):
+#     print("*"*i)
+#     i += 1
+
+# while(True):
+#     try:
+#         A, B = map(int, input().split())
+#         print(A+B)
+#     except:
+#         break
