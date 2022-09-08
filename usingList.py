c8a7a0f0a4313e658734679a35b32ba9bf4ecb5d
Justin_Kim <65004984+justin-kim3000@users.noreply.github.com>
2022-09-08 10:11:21 +0900

    Delete usingList.py

--- usingList.py
+++
@@ -1,8 +0,0 @@
-# M 보다 작은 수 출력
-# N, M = map(int, input().split())
-# list_N = list(map(int, input().split()))
-
-
-# for i in range(0, N):
-#     if(list_N[i] < M):
-#         print(list_N[i], end=" ")
