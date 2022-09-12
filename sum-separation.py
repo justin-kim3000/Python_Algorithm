0ea98ea0cbcca0afd717de66da1b10806c180e84
justin-kim3000 <suc40@naver.com>
2022-09-12 23:33:54 +0900

    ing

--- sum-separation.py
+++ sum-separation.py
@@ -1,11 +1,26 @@
-inputNumber = str(input())
+# 분해합 구하는 코드
+# inputNumber = str(input())
+
+# def sumSeparation(number):
+#     temp = 0
+#     for i in number:
+#         temp += int(i)
+#     print(temp+int(number))
 
 
-def sumSeparation(number):
-    temp = 0
-    for i in number:
-        temp += int(i)
-    print(temp+int(number))
+# sumSeparation(inputNumber)
 
+# 생성자 구하는 코드
+inputNumber = int(input())
+result = 0
 
-sumSeparation(inputNumber)
+for i in range(1, inputNumber+1):
+    nums = list(map(int, str(i)))
+    print(nums)
+    result = sum(nums) + i
+    print(result)
+    if result == inputNumber:
+        print(i)
+        break
+    if i == inputNumber:
+        print(0)
