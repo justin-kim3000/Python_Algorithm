4638fe32050720fc10c7c9bd4f45adb713c570be
Justin Kim <suc40@naver.com>
2022-09-14 15:51:21 +0900

    done

--- blackJack.py
+++ blackJack.py
@@ -1,13 +1,14 @@
 getCard, maxSum = map(int, input().split())
 cardCollection = list(map(int, input().split()))
-result = []
 sum1 = 0
 
+# 어짜피 3번 안에 완료하면 됨
 for i in range(getCard):
-    if max(cardCollection) + sum1 < maxSum:
-        maxList = max(cardCollection)
-        popList = cardCollection.remove(maxList)
-        sum1 += maxList
-# result.append(popList)
-
+    for j in range(i+1, getCard):
+        for k in range(j+1, getCard):
+            if(cardCollection[i]+cardCollection[j]+cardCollection[k] > maxSum):
+                continue
+            else:
+                sum1 = max(
+                    sum1, cardCollection[i]+cardCollection[j]+cardCollection[k])
 print(sum1)
