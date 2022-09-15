89f4f824161d87cb864c9d1a0d3ca5cabd332db9
Justin Kim <suc40@naver.com>
2022-09-15 16:14:23 +0900

    done

--- sort-point.py
+++ sort-point.py
@@ -1,9 +1,20 @@
 N = int(input())
 numList = []
+# X축 정렬
+# for i in range(N):
+#     x, y = list(map(int, input().split()))
+#     numList.append([x][y])
+
+# numList.sort()
+
+# for i in range(N):
+#     print(numList[i][0], numList[i][1])
+
+# Y축 정렬
 for i in range(N):
-    numList.append(list(map(int, input().split())))
+    x, y = list(map(int, input().split()))
+    numList.append([y, x])
 
 numList.sort()
-
 for i in range(N):
-    print(numList[i][0], numList[i][1])
+    print(numList[i][1], numList[i][0])
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,4 @@
-hile(1):
-#     if len(Number1) == 0:
-#         break
-#     else:
-#         temp.append(Number1[K-1])
-#  
+
+
+for i in range(N):
+    print(numList[i][0], numList[i][1])
