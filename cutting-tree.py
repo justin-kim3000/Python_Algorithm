c26ef28603ee843ba7155707163aa51e9d9bfed0
Justin Kim <suc40@naver.com>
2022-09-16 15:04:57 +0900

    study more

--- cutting-tree.py
+++ cutting-tree.py
@@ -2,33 +2,53 @@
 # treeLength를 집에 가져가기 위해 절단기 최대 설정 높이?
 # 아이디어 : treeHeight의 높이를 각각 1씩 잘라서 잘라진 합이 takeLength와 같은지 비교를 한다. 만약 같다면 break 그뒤 주어진 나무에서 가장 높은 높이를 출력한다.
 
-# treeCount, takeLength = map(int, input().split())
-treeCount, takeLength = 4, 7
+treeCount, takeLength = map(int, input().split())
 
-# treeHeight = list(map(int, input().split()))
-treeHeight = [20, 15, 10, 17]
-count = 0
-print(treeHeight)
+treeHeight = list(map(int, input().split()))
 
+start, end = 1, sum(treeHeight)
 
-start, end = 1, max(treeHeight)  # 이분탐색 검색 범위 설정
-
-while start <= end:  # 적절한 벌목 높이를 찾는 알고리즘
+while start <= end:
     mid = (start+end) // 2
-
-    log = 0  # 벌목된 나무 총합
+    log = 0
     for i in treeHeight:
-        if i >= mid:
+        if i > mid:
             log += i - mid
 
-    # 벌목 높이를 이분탐색
     if log >= takeLength:
         start = mid + 1
     else:
         end = mid - 1
 print(end)
+# ---------------------------------------
+# 시간 안에 푼 코드.
+# import sys
+# input= sys.stdin.readline
 
+# def binary_search(arr, start, end):
+#     res = 0
+#     while start <= end:
+#         mid = (start+end) // 2
+#         total = 0
 
+#         for x in arr:
+#             if x > mid:
+#                 total+= x- mid
+
+#         if total < m:
+#             end = mid-1
+#         else:
+#             res = mid
+#             start = mid +1
+#     return res
+
+# n , m = map(int,input().split())
+# li = list(map(int,input().split()))
+
+# r = binary_search(li, 0, max(li))
+# print(r)
+
+# -----------------------------------------------------
 # treeHeight.index(max(treeHeight))-1
 # print(treeHeight[treeHeight.index(max(treeHeight))]-1)
 # print(treeHeight)
