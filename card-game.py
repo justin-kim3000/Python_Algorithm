c1c00735d2a1a561a36e11896e2a2e3dccdae807
justin-kim3000 <suc40@naver.com>
2022-09-18 20:22:13 +0900

    fast

--- card-game.py
+++ card-game.py
@@ -1,11 +1,21 @@
+from collections import deque
+import sys
+
+countCard = int(sys.stdin.readline())
+useDeque = deque()
 # time out.....
-countCard = int(input())
-cardList = list(map(int, range(1, countCard+1)))
-while 1:
-    if len(cardList) == 1:
-        print(cardList)
-        break
-    else:
-        cardList.pop(0)
-        cardList.append(cardList[0])
-        cardList.pop(0)
+# cardList = list(map(int, range(1, countCard+1)))
+# while 1:
+#     if len(cardList) == 1:
+#         print(cardList)
+#         break
+#     else:
+#         cardList.pop(0)
+#         cardList.append(cardList[0])
+#         cardList.pop(0)
+for i in range(countCard):
+    useDeque.append(i+1)
+while len(useDeque) > 1:
+    useDeque.popleft()
+    useDeque.append(useDeque.popleft())
+print(useDeque.pop())
