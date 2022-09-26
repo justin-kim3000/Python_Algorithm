0e0a2c61e9607e0228720a7cda74b5708e99c097
Justin Kim <suc40@naver.com>
2022-09-26 12:29:32 +0900

    use-heapq

--- heap/heapq-max(11279).py
+++ heap/heapq-max(11279).py
@@ -3,12 +3,13 @@ import sys
 
 x = int(sys.stdin.readline())
 heap = []
+
 for i in range(x):
     num = int(sys.stdin.readline())
     if num != 0:
-        heapq.heappush(heap, num)
+        heapq.heappush(heap, -num)
     else:
         if heap:
-            print(heapq.heappop(heap))
+            print(-1*heapq.heappop(heap))
         else:
             print(0)
