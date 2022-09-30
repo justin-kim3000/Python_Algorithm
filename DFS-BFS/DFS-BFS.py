c0e16ecd819127f0bb0e339887c7c8c1d2f58b5b
Justin Kim <suc40@naver.com>
2022-09-30 10:51:34 +0900

    ing

--- DFS-BFS/DFS-BFS.py
+++ DFS-BFS/DFS-BFS.py
@@ -1,26 +1,34 @@
+import sys
+from collections import deque
 
-# N,M,V = map(int,input().split())
-N,M,V = 5,5,3
+# N -> 정점의 개수
+# M -> 간선의 개수
+# V -> 시작할 정점의 번호
+
+N, M, V = map(int, input().split())
+
 # ---------참고 자료 -----------#
-#행렬 만들기
-graph = [[0]*(N+1) for _ in range(N+1)]
-print(graph)
-for i in range (M):
-    a,b = map(int,input().split())
-    graph[a][b] = graph[b][a] = 1
-print(graph)
+# 행렬 만들기
+# graph = [[0]*(N+1) for _ in range(N+1)]
+# print(graph)
+# for i in range(M):
+#     a, b = map(int, input().split())
+#     graph[a][b] = graph[b][a] = 1
+# print(graph)
 
-#방문 리스트 행렬
-visited1 = [0]*(N+1)
-visited2 = visited1.copy()
+# # 방문 리스트 행렬
+# visited1 = [0]*(N+1)
+# visited2 = visited1.copy()
 
-#dfs 함수 만들기
-def dfs(V):
-    visited1[V] = 1 #방문처리
-    print(V, end=' ')
-    for i in range(1, N+1):
-        if graph[V][i] == 1 and visited1[i] == 0:
-            dfs(i)
+# dfs 함수 만들기
+
+
+# def dfs(V):
+#     visited1[V] = 1  # 방문처리
+#     print(V, end=' ')
+#     for i in range(1, N+1):
+#         if graph[V][i] == 1 and visited1[i] == 0:
+#             dfs(i)
 
 # #bfs 함수 만들기
 # def bfs(V):
@@ -34,7 +42,46 @@ def dfs(V):
 #                 queue.append(i)
 #                 visited2[i] = 1 # 방문처리
 
-dfs(V)
+# dfs(V)
 # print()
 # bfs(V)
-#-----------------------------------
+# -----------------------------------
+
+# ----------- 참고자료2 ---------------
+def dfs(n):
+    visited[n] = True
+    print(n, end=' ')
+    for i in graph[n]:
+        if not visited[i]:
+            dfs(i)
+
+
+def bfs(n):
+    queue = deque([n])
+    visited[n] = True
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+
+graph = [[] for _ in range(N+1)]
+for i in range(M):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+print(graph)
+for i in range(1, N+1):
+    graph[i].sort()
+
+print(graph)
+
+visited = [False]*(N+1)
+dfs(V)
+print()
+visited = [False]*(N+1)
+bfs(V)
