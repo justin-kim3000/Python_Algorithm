5e913b0bd235e69e0cf17103518101409a3b8ad7
Justin Kim <suc40@naver.com>
2022-10-02 19:01:23 +0900

    thinkmore

--- RecursiveFunction/quadTree(1992).py
+++ RecursiveFunction/quadTree(1992).py
@@ -1,31 +1,60 @@
 # 자식이 4개인 트리
+# 예시1
+# N = int(input())
+# graph = [list(map(int, input())) for _ in range(N)]
 
+
+# def quad(x, y, n):
+#     check = graph[x][y]
+
+#     for i in range(x, x+n):
+#         for j in range(y, y+n):
+#             if check != graph[i][j]:
+#                 check = -1
+#                 break
+
+#     if check == -1:
+#         print("(", end='')
+#         n //= 2
+#         quad(x, y, n)
+#         quad(x, y+n, n)
+#         quad(x+n, y, n)
+#         quad(x+n, y+n, n)
+#         print(")", end='')
+
+#     elif check == 1:
+#         print(1, end='')
+#     else:
+#         print(0, end='')
+
+
+# quad(0, 0, N)
+
+# 예제 2
 N = int(input())
+
 graph = [list(map(int, input())) for _ in range(N)]
 
 
-def quad(x, y, n):
-    check = graph[x][y]
+def quad2(x, y, n):
+    if n == 1:
+        return str(graph[x][y])
+
+    result = []
 
     for i in range(x, x+n):
         for j in range(y, y+n):
-            if check != graph[i][j]:
-                check = -1
-                break
+            if graph[i][j] != graph[x][y]:
+                n //= 2
+                result.append('(')
+                result.extend(quad2(x, y, n))
+                result.extend(quad2(x, y+n, n))
+                result.extend(quad2(x+n, y, n))
+                result.extend(quad2(x+n, y+n, n))
+                result.append(')')
 
-    if check == -1:
-        print("(", end='')
-        n //= 2
-        quad(x, y, n)
-        quad(x, y+n, n)
-        quad(x+n, y, n)
-        quad(x+n, y+n, n)
-        print(")", end='')
-
-    elif check == 1:
-        print(1, end='')
-    else:
-        print(0, end='')
+                return result
+    return str(graph[x][y])
 
 
-quad(0, 0, N)
+print("".join(quad2(0, 0, N)))
