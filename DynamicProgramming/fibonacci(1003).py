767740cee0a39d2d701c209a8c9191a7b3c82855
Justin Kim <suc40@naver.com>
2022-10-04 12:30:20 +0900

    use-rule

--- DynamicProgramming/fibonacci(1003).py
+++ DynamicProgramming/fibonacci(1003).py
@@ -1,8 +1,8 @@
 # 1번 방법 - 시간초과
-import sys
-T = int(sys.stdin.readline())
-count_zero = 0
-count_one = 0
+# import sys
+# T = int(sys.stdin.readline())
+# count_zero = 0
+# count_one = 0
 
 
 def fibonacci(N):
@@ -17,9 +17,24 @@ def fibonacci(N):
         return fibonacci(N-1) + fibonacci(N-2)
 
 
+# for _ in range(T):
+#     num1 = int(sys.stdin.readline())
+#     fibonacci(num1)
+#     print(count_zero, count_one)
+#     count_zero = 0
+#     count_one = 0
+
+# 2번 방법 - 규칙 찾기
+# 위 피보나치 식에 값을 1~9 까지 넣으보면 해당 값도 피보나치 수열을 따름을 알 수 있다.
+
+T = int(input())
 for _ in range(T):
-    num1 = int(sys.stdin.readline())
-    fibonacci(num1)
-    print(count_zero, count_one)
-    count_zero = 0
+    num = int(input())
+    count_zero = 1
     count_one = 0
+    if num == 0:
+        print(count_zero, count_one)
+    else:
+        for _ in range(num):
+            count_zero, count_one = count_one, count_one + count_zero
+        print(count_zero, count_one)
---
+++ DynamicProgramming/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+count_zero = 0
+# count_one = 0
