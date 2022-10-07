bb70b389ecbc0b9f8b3b95ad48834ac205ad272c
Justin Kim <suc40@naver.com>
2022-10-07 09:21:48 +0900

    ing

--- DynamicProgramming/FourSquares(17626).py
+++ DynamicProgramming/FourSquares(17626).py
@@ -2,10 +2,22 @@ from math import sqrt
 
 n = int(input())
 countNum = 0
+check = 0
+room = [i**2 for i in range(1, n+1)]
+print(room)
 
-if type(sqrt(n)) is float:
-    countNum += 1
-
-
-print(countNum)
-print(sqrt(n))
+if sqrt(n)**2 == n:
+    print(1)
+else:
+    for i in range(n-1, 0, -1):
+        check += room[i]
+        if n < check:
+            countNum += 1
+        elif n == check:
+            countNum += 1
+            break
+        elif n > check:
+            check -= room[i]
+        else:
+            countNum = 0
+    print(countNum)
