1e1b643ef33a78df2fb07066a4c1068eeb560534
Justin Kim <suc40@naver.com>
2022-10-09 19:07:17 +0900

    done

---
+++ DataStructure/nojab(1764).py
@@ -0,0 +1,17 @@
+N, M = map(int, input().split())
+li1 = set()
+li2 = set()
+
+for _ in range(N):
+    li1.add(input())
+
+for _ in range(M):
+    li2.add(input())
+
+print(li1)
+print(li2)
+
+res = sorted(list(li1 & li2))
+print(len(res))
+for i in res:
+    print(i)
