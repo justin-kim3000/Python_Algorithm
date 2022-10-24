34d09cea974b4a6f679e17c9ddac7524f3cd3eb3
Justin Kim <suc40@naver.com>
2022-10-24 14:09:13 +0900

    think-more

--- DynamicProgramming/backpack(12865).py
+++ DynamicProgramming/backpack(12865).py
@@ -10,19 +10,30 @@
 # for key in weight:
 #     print(key)
 
-n, k = map(int, input().split())  # n: 물품의 수, k: 가방에 넣을 수 있는 최대 무게
-dp = [[0] * (k+1) for _ in range(n+1)]
+
+# n: 물품의 수, k: 가방에 넣을 수 있는 최대 무게
+n, k = map(int, input().split())
+# 비교 배낭
+pock = [[0] * (k+1) for _ in range(n+1)]
 m = [[0, 0]]
+
 for i in range(n):
-    w, v = map(int, input().split())  # w: 각 물건의 무게, v: 물건의 가치
+    # w: 물건 무게, v: 물건 가치
+    w, v = map(int, input().split())
     m.append([w, v])
 
+# [0,0]뒤에 붙으므로 범위를 1부터 n+1로 정함
 for i in range(1, n+1):
+    # 무게
     w = m[i][0]
+    # 가치
     v = m[i][1]
+    # k : 최대무게
     for j in range(1, k+1):
+        # 최대무게 보다 무거우면 물품수를 뺌
         if j < w:
-            dp[i][j] = dp[i-1][j]
+            pock[i][j] = pock[i-1][j]
+        # 최대무게보다 적을 경우, 가치와 무게중 최대값
         else:
-            dp[i][j] = max(dp[i-1][j-w] + v, dp[i-1][j])
-print(dp[n][k])
+            pock[i][j] = max(pock[i-1][j-w] + v, pock[i-1][j])
+print(pock[n][k])
