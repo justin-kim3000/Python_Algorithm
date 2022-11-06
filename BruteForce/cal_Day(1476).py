481f55d9d1260801c6e36b2d5fe6016aed747fe6
Justin Kim <suc40@naver.com>
2022-11-06 20:12:27 +0900

    time out

--- BruteForce/cal_Day(1476).py
+++ BruteForce/cal_Day(1476).py
@@ -1,5 +1,24 @@
 # 1~15:E, 1~28:S, 1~19:M
 E, S, M = map(int, input().split())
-print(E, S, M)
-
+tempE = 1
+tempS = 1
+tempM = 1
 cou = 1
+
+while(1):
+    if tempE == E and tempS == S and tempM == M:
+        print(cou)
+        break
+    elif tempE % 15 == 0:
+        tempE = 0
+    elif tempS % 28 == 0:
+        tempS = 0
+    elif tempM % 19 == 0:
+        tempM = 0
+
+    tempM += 1
+    tempS += 1
+    tempE += 1
+    print(tempM, tempS, tempE)
+
+    cou += 1
