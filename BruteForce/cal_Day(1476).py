1cca9f3498c4b7af7cff9fbbf75c39f10418d8e0
Justin Kim <suc40@naver.com>
2022-11-06 20:25:08 +0900

    save time

--- BruteForce/cal_Day(1476).py
+++ BruteForce/cal_Day(1476).py
@@ -7,18 +7,32 @@ cou = 1
 
 while(1):
     if tempE == E and tempS == S and tempM == M:
-        print(cou)
         break
-    elif tempE % 15 == 0:
-        tempE = 0
-    elif tempS % 28 == 0:
-        tempS = 0
-    elif tempM % 19 == 0:
-        tempM = 0
-
     tempM += 1
     tempS += 1
     tempE += 1
-    print(tempM, tempS, tempE)
-
     cou += 1
+
+    if tempE > 15:
+        tempE -= 15
+    if tempS > 28:
+        tempS -= 28
+    if tempM > 19:
+        tempM -= 19
+print(cou)
+# while(1):
+#     if tempE == E and tempS == S and tempM == M:
+#         break
+#     if tempE % 15 == 0:
+#         tempE = 0
+#     elif tempS % 28 == 0:
+#         tempS = 0
+#     elif tempM % 19 == 0:
+#         tempM = 0
+
+#     tempM += 1
+#     tempS += 1
+#     tempE += 1
+
+#     cou += 1
+# print(cou)
