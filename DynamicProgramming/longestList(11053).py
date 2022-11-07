082f4a3c9deef5bcc74b4a428ceb56f2bd7d5455
Justin Kim <suc40@naver.com>
2022-11-07 10:57:22 +0900

    ing

--- DynamicProgramming/longestList(11053).py
+++ DynamicProgramming/longestList(11053).py
@@ -1,14 +1,10 @@
 # 가장 증가하는 부분 수열 구하시오.
 
 N = int(input())
-cou = 1
+cou = 0
 numlist = list(map(int, input().split()))
 temp = 0
-
-print(max(numlist[:3]))
-
-
-# for i in range(1, N):
-#     if numlist[i] > max(numlist[:i-1]):
-#         cou += 1
+for i in range(N):
+    if numlist[i] >= max(numlist[:i+1]):
+        cou += 1
 print(cou)
