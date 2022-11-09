40fa1a0e4167a25e233825e40195c3df503ad008
Justin Kim <suc40@naver.com>
2022-11-09 21:32:46 +0900

    while

--- GreedyAlgorithm/AtoB(16953).py
+++ GreedyAlgorithm/AtoB(16953).py
@@ -1,14 +1,15 @@
 start, end = map(int, input().split())
 counting = 1
 # top to bottom
-for _ in range(10):
-    if end % 2 != 0 and str(end[-1]) == '1':
-        end = int(str(end)-'1')
+while(start != end):
+    if end % 2 != 0 and str(end)[-1] == '1':
+        end = int(str(end)-1)
         counting += 1
     elif end % 2 == 0:
-        end %= 2
+        end /= 2
         counting += 1
     else:
         print(-1)
+        break
 
 print(counting)
