a8f1296b713873ccae6c4102b0a640d801a4f316
Justin Kim <suc40@naver.com>
2022-11-14 16:33:56 +0900

    minus is sign of break

--- etc/bracket(9012).py
+++ etc/bracket(9012).py
@@ -1,8 +1,26 @@
+# N = int(input())
+
+# for _ in range(N):
+#     temp = list(map(str, input()))
+#     if temp.count('(') == temp.count(')'):
+#         print("YES")
+#     else:
+#         print("NO")
 N = int(input())
 
 for _ in range(N):
     temp = list(map(str, input()))
-    if temp.count('(') == temp.count(')'):
-        print("YES")
-    else:
+    sumN = 0
+
+    for i in temp:
+        if i == '(':
+            sumN += 1
+        elif i == ')':
+            sumN -= 1
+        if sumN < 0:
+            print("NO")
+            break
+    if sumN > 0:
         print("NO")
+    elif sumN == 0:
+        print("YES")
