1ab56b9dc37a7b8c946359888daf99857f5ea105
Justin Kim <suc40@naver.com>
2022-11-20 10:08:01 +0900

    ing

--- Backtracking/Password(1759).py
+++ Backtracking/Password(1759).py
@@ -1,18 +1,43 @@
+# L, C = map(int, input().split())
+
+# password = list(map(str, input().split()))
+
+# password = sorted(password)
+# print(password)
+
+# end = ""
+# for i in range(C-L, C):
+#     end += password[i]
+
+# while(1):
+#     temp = ""
+#     for i in range(L):
+#         temp += password[i]
+#     print(temp)
+#     if temp == end:
+#         break
+
+# othrer
+
 L, C = map(int, input().split())
 
 password = list(map(str, input().split()))
-
 password = sorted(password)
-print(password)
 
-end = ""
-for i in range(C-L, C):
-    end += password[i]
+vowels = ['a','e','i','o','u']
 
-while(1):
-    temp = ""
-    for i in range(L):
-        temp += password[i]
-    print(temp)
-    if temp == end:
-        break
+
+def back_tracking(count,idx):
+    if count == L:
+        vo = 0
+        co = 0
+        for i in range(L):
+            if password[i] in vowels:
+                vo +=1
+            else:
+                co +=1
+        if vo >= 1 and co >=2:
+            print("".join(password))
+        return
+    # for i in range(idx, C):
+        
