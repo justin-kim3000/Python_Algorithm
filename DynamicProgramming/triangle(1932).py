c5634e63982e390b9732e718855b4dcc424f1060
Justin Kim <suc40@naver.com>
2022-11-28 20:40:44 +0900

    ing

--- DynamicProgramming/triangle(1932).py
+++ DynamicProgramming/triangle(1932).py
@@ -1,1 +1,10 @@
 n = int(input())
+num_li = []
+
+for i in range(n):
+    num_li.append(list(map(int,input().split())))
+    
+for i in range(1,n):
+    for j in range(len(num_li[i])):
+        if j == 0:
+            num_li[i][j] = num_li[i][j]+num_li[i-1][j]
