179ed1a1f030e6e1b9b5acb16272f40ddf97f5d5
Justin Kim <suc40@naver.com>
2022-12-08 13:55:15 +0900

    little change

---
+++ DataStructure/deque(10866).py
@@ -0,0 +1,45 @@
+import sys
+from collections import deque
+N = int(sys.stdin.readline())
+stack = deque([])
+
+for i in range(N):
+    command = sys.stdin.readline().split()
+    if command[0] == "push_back":
+        stack.append(command[1])
+    elif command[0] == "push_front":
+        stack.appendleft(command[1])
+        
+    elif command[0] == "pop_front":
+        if len(stack) ==0:
+            print(-1)  
+        else:
+            print(stack.popleft())
+            
+    elif command[0] == "pop_back":
+        if len(stack) ==0:
+            print(-1)  
+        else:
+            print(stack.pop())
+    elif command[0] == "size":
+        print(len(stack))
+    elif command[0] == "empty":
+        if len(stack)==0:
+            print(1)
+        else:
+            print(0)
+
+    elif command[0] == "front":
+        if len(stack)==0:
+            print(-1)
+        else:
+            print(stack[0])
+
+    elif command[0] == "back":
+        if len(stack)==0:
+            print(-1)
+        else:
+            print(stack[-1])  
+        
+    
+    
