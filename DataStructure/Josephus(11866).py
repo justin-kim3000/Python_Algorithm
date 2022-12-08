9e38e9ca2d104c373d11e920d0cae2f8f10c4aaa
Justin Kim <suc40@naver.com>
2022-12-08 12:37:46 +0900

    use deque

--- DataStructure/Josephus(11866).py
+++ DataStructure/Josephus(11866).py
@@ -1,19 +1,16 @@
-
 from collections import deque
 
 
 N, K = map(int, input().split())
 
-# 아이디어 : 리스트를 이용해 1부터 N까지 행렬을 만들고, K배수의 index를 추출 후 지운다. (조건 : index번호가 N을 넘어갈때 다시 돌아와서 카운트 한다.)
-Number1 = list(map(int, range(1, N+1)))
-temp = deque([])
+Number1 = deque([i for i in range(1,N+1)])
+print('<',end='')
+while(Number1):
+    for _ in range(K-1):
+        Number1.append(Number1[0])
+        Number1.popleft()
+    print(Number1.popleft(),end='')
+    if Number1:
+        print(', ',end='')
 
-while(1):
-    if len(Number1) == 0:
-        break
-    else:
-        temp.append(Number1[K-1])
-        del Number1[K-1]
-
-
-print(temp)
+print('>',end='')
