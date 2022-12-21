7e95de2ada795d75c5d5bfc0be3607f25fffa64d
Justin Kim <suc40@naver.com>
2022-12-21 23:41:32 +0900

    idea recursive

--- DynamicProgramming/willbe(2775).py
+++ DynamicProgramming/willbe(2775).py
@@ -1,7 +1,15 @@
-test = int(input())
+# test = int(input())
 # 0층에 i번째 -> i명
 # 비율로 증가
-for _ in range(test):
-    k = int(input())
-    t = int(input())
+
+def sol(n):
+    if n == 1:
+        return 1
+    return n+sol(n-1)
+
+print(sol(10))
+
+# for _ in range(test):
+#     k = int(input())
+#     t = int(input())
     
