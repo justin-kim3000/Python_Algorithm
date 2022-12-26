0c4fb52a6dac20dcee2e45801a708d712e6a5ae1
Justin Kim <suc40@naver.com>
2022-12-26 23:12:22 +0900

    ing

--- DynamicProgramming/sticker(9465).py
+++ DynamicProgramming/sticker(9465).py
@@ -1,10 +1,6 @@
-# T = int(input())
+T = int(input())
 
-room = [[list(map(int,input().split()))] for _ in range(2)]
-print(room)
-# for _ in range(T):
-#     n = int(input())
-#     room = [0 for _ in range(n)]*2
-#     for i in range(2):
-#         room = list(map(int,input().split()))
-        
+for _ in range(T):
+    n = int(input())
+    room = [[list(map(int,input().split()))] for _ in range(2)]
+    
