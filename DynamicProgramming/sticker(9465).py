5ee6c0e203dcc77fb693adfdf1b390afbf534b7f
Justin Kim <suc40@naver.com>
2022-12-27 23:37:51 +0900

    think more

--- DynamicProgramming/sticker(9465).py
+++ DynamicProgramming/sticker(9465).py
@@ -2,5 +2,12 @@ T = int(input())
 
 for _ in range(T):
     n = int(input())
-    room = [[list(map(int,input().split()))] for _ in range(2)]
-    
+    room = [list(map(int,input().split())) for _ in range(2)]
+    for j in range(1, n):
+        if j == 1:
+            room[0][j] += room[1][j - 1]
+            room[1][j] += room[0][j - 1]
+        else:
+            room[0][j] += max(room[1][j - 1], room[1][j - 2])
+            room[1][j] += max(room[0][j - 1], room[0][j - 2])
+    print(max(room[0][n - 1], room[1][n - 1]))
