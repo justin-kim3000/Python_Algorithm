32ae20430474adb09a953f6f5856ac82da186fe8
Justin Kim <suc40@naver.com>
2022-12-28 23:32:32 +0900

    use pop

--- DataStructure/zero(10773).py
+++ DataStructure/zero(10773).py
@@ -1,9 +1,14 @@
+from collections import deque
+
+
 K = int(input())
-li = []
+li = deque()
+
 for _ in range(K):
-    li.append(int(input()))
+    temp = int(input())
+    if temp != 0:
+        li.append(temp)
+    else:
+        li.pop()
     
-for i in range(len(li)):
-    if li[i] == 0:
-        li[i-1] = 0
 print(sum(li))
