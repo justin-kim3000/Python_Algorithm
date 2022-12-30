e2071e860f1de43dfddf8465f89605ccf803ae99
Justin Kim <suc40@naver.com>
2022-12-30 23:44:20 +0900

    think more

--- etc/statistics(2108).py
+++ etc/statistics(2108).py
@@ -1,13 +1,26 @@
 # N은 홀수
-N = int(input())
+from collections import Counter
+import sys
+N = int(sys.stdin.readline())
 li = []
 
 for _ in range(N):
-    li.append(int(input()))
+    li.append(int(sys.stdin.readline()))
 
 print("----------------")
 print(round(sum(li)/N))
 print(li[round(len(li)/2-1)])
+
+cnt = Counter(li).most_common(2)
+
+if len(li) >1:
+    if cnt[0][1] == cnt[1][1]:
+        print(cnt[0][1])
+    else:
+        print(cnt[0][0])
+else:
+    print(cnt[0][0])
+
 
 print()
 print(max(li)-min(li))
