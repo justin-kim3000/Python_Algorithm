062b819127e5d1de35abb681ee02f9907085b400
Justin Kim <suc40@naver.com>
2022-12-30 23:52:20 +0900

    done

--- etc/statistics(2108).py
+++ etc/statistics(2108).py
@@ -7,20 +7,19 @@ li = []
 for _ in range(N):
     li.append(int(sys.stdin.readline()))
 
-print("----------------")
+
+li = sorted(li)
+
 print(round(sum(li)/N))
-print(li[round(len(li)/2-1)])
+print(li[len(li)//2])
 
 cnt = Counter(li).most_common(2)
-
 if len(li) >1:
     if cnt[0][1] == cnt[1][1]:
-        print(cnt[0][1])
+        print(cnt[1][0])
     else:
         print(cnt[0][0])
 else:
     print(cnt[0][0])
 
-
-print()
 print(max(li)-min(li))
