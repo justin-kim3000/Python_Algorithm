d77d92361b32af64f8f6e8adb15f01a579360378
Justin Kim <suc40@naver.com>
2023-01-03 21:42:11 +0900

    ing

--- DataStructure/balance(4949).py
+++ DataStructure/balance(4949).py
@@ -1,6 +1,15 @@
 # '['이면 ']'로 끝나야 하며, '('이면 ')'로 끝이 나야한다.
 # 아이디어 [로 시작하고 ]로 끝나는 경우 참.
-li = []
-li.append(input())
+# li = []
+# li.append(input())
 
-print(li)
+# print(li)
+
+while True :
+    a = input()
+    stack = []
+
+    if a == "." :
+        break
+    for i in a :
+        print(i,end='\n\n')
