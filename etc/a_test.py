cfe26ec89281bc4e3c227ab4f581a1dffdfdc616
Justin Kim <suc40@naver.com>
2023-01-13 10:18:45 +0900

    class

--- etc/a_test.py
+++ etc/a_test.py
@@ -1,7 +1,15 @@
 import aboutPython_Class
+import random
 
-ab = aboutPython_Class
+# ab = aboutPython_Class
 
-car = ab.Car4("기아","k7","블랙","사천만원")
+# car = ab.Car4("기아","k7","블랙","사천만원")
 
-car.carInfoShow()
+# car.carInfoShow()
+
+print(dir(random))
+for _ in range(2000):
+    rd = random.randint(1,199)
+    print(rd)
+    if rd == 100:
+        break
