2016603d471e1178e0dbb3092e6461e03e9f1784
Justin Kim <suc40@naver.com>
2023-03-05 23:37:23 +0900

    ing

---
+++ etc/union(11723).py
@@ -0,0 +1,25 @@
+S = []
+M = map(int,input())
+for i in range(M):
+    word, num = input().split()
+    if word == 'add':
+        if num not in S:
+            S.append(num)
+    if word == 'remove':
+        if num in S:
+            S.remove(num)
+    if word == 'check':
+        if num in S:
+            print(1)
+        else:
+            print(0)
+    if word == 'toggle':
+        if num in S:
+            S.remove(num)
+        else:
+            S.append(num)
+    if word == 'all':
+        S = []
+        S = [i for i in range(1,21)]
+    if word == 'empty':
+        S = []
