f1f4a22ce93f5e9a3c3e4a8376ac5f6e06a0b0e2
Justin Kim <suc40@naver.com>
2023-04-04 23:57:45 +0900

    think

--- DataStructure/yosahp(1158).py
+++ DataStructure/yosahp(1158).py
@@ -1,9 +1,13 @@
 N, K = map(int, input().split())
+arr = [i for i in range(1, N+1)]
 
-for i in range(1, N):
-    if K*i <= N:
-        print(f'{K*i},', end=' ')
-    elif N-K*i < 0:
-        print(f'{-(N-K*i)},', end=' ')
-    else:
-        print(f'{N-K*i},', end=' ')
+answer = []
+num = 0
+
+for t in range(N):
+    num += K-1
+    if num >= len(arr):
+        num = num % len(arr)
+
+    answer.append(str(arr.pop(num)))
+print("<", ", ".join(answer)[:], ">", sep='')
